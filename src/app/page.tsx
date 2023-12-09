cb08483b897d3a1fef8afea2aae360ecdd4abf40
Ivan Tanaka <ivantanaka@Ivans-MacBook-Air.local>
2023-12-09 12:34:30 +0700

    feature: adding external links, adding meta tags

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 function Home() {
   return (
@@ -20,24 +21,42 @@ function Home() {
         so feel free to reach out and connect with me!
       </p>
       <div className="flex gap-2 mt-4">
-        <Image
-          alt="whatsapp"
-          src="/assets/images/whatsapp-icon.svg"
-          width={35}
-          height={35}
-        />
-        <Image
-          alt="linkedin"
-          src="/assets/images/linkedin-icon.svg"
-          width={35}
-          height={35}
-        />
-        <Image
-          alt="github"
-          src="/assets/images/github-icon.svg"
-          width={35}
-          height={35}
-        />
+        <a
+          href={"https://wa.link/nf8yeb"}
+          target={"_blank"}
+          rel={"noreferrer noopener"}
+        >
+          <Image
+            alt="whatsapp"
+            src="/assets/images/whatsapp-icon.svg"
+            width={35}
+            height={35}
+          />
+        </a>
+        <a
+          href={"https://www.linkedin.com/in/ivantanakaa/"}
+          target={"_blank"}
+          rel={"noreferrer noopener"}
+        >
+          <Image
+            alt="linkedin"
+            src="/assets/images/linkedin-icon.svg"
+            width={35}
+            height={35}
+          />
+        </a>
+        <a
+          href={"https://github.com/IvanTanaka"}
+          target={"_blank"}
+          rel={"noreferrer noopener"}
+        >
+          <Image
+            alt="github"
+            src="/assets/images/github-icon.svg"
+            width={35}
+            height={35}
+          />
+        </a>
       </div>
     </div>
   );
