d99b30342958a3ab276c68a3275e6c566ae07fc9
Ivan Tanaka <ivantanaka@Ivans-MacBook-Air.local>
2024-04-11 21:47:44 +0700

    feat: adding download cv feature

---
+++ src/app/components/Navigation/index.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Navigation() {
+  return (
+    <div className="fixed right-2 xl:right-10 bottom-2 xl:bottom-10">
+      <Link
+        download={"Ivan Tanaka CV"}
+        target="_blank"
+        href="/assets/pdf/Ivan Tanaka CV.pdf"
+        locale={false}
+      >
+        <div className="bg-[#3d3d3d] rounded-full p-2">
+          <Image
+            alt="whatsapp"
+            src="/assets/images/file-download-icon.svg"
+            width={35}
+            height={35}
+          />
+        </div>
+      </Link>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,17 +3,20 @@ import Skills from "./components/home/Skills";
 import Careers from "./components/home/Careers";
 import Educations from "./components/home/Educations";
 import Honors from "./components/home/Honors";
+import Navigation from "./components/Navigation";
 
 function Home() {
   return (
-    <>
+    <div className="relative">
+      <Navigation />
       <Hero />
       <Careers />
       <Educations />
       <div className="flex flex-col lg:flex-row lg:w-max lg:m-auto">
         <Skills /> <Honors />
       </div>
-    </>
+      <div className="mb-10" />
+    </div>
   );
 }
 export default Home;
