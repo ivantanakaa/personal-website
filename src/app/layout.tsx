bd82ff8c78da9abb8ed6ad2292dec8cebb42f2d9
Ivan Tanaka <ivantanaka@Ivans-MacBook-Air.local>
2024-04-12 20:48:20 +0700

    feat: installing google tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -34,6 +36,7 @@ export default function RootLayout({
         <link rel="icon" href="/favicon.ico" sizes="any" />
       </head>
       <body className={inter.className}>{children}</body>
+      <GoogleAnalytics gaId="G-S6KXB9Y97V" />
     </html>
   );
 }
