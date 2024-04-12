132f561bbc013aea2ca0912fd6ecbc0c339d342b
Ivan Tanaka <ivantanaka@Ivans-MacBook-Air.local>
2024-04-12 15:10:33 +0700

    fix: career position style

--- src/app/components/home/Careers/index.tsx
+++ src/app/components/home/Careers/index.tsx
@@ -28,7 +28,7 @@ export default function Careers() {
               className="flex flex-col justify-start my-4"
               key={`career-${index}`}
             >
-              <h2 className=" font-medium text-2xl mb-2 w-fit">
+              <h2 className=" font-medium text-2xl w-fit">
                 {career.corporate_link ? (
                   <Link
                     className="text-blue-500 hover:text-blue-700 hover:transition-all delay-150"
@@ -41,7 +41,7 @@ export default function Careers() {
                 ) : (
                   career.corporate
                 )}
-                <span className=" border-l-4 ml-4 pl-4 h-100 border-[#3d3d3d]">
+                <span className="block mt-2 md:mt-0 text-lg md:text-2xl md:inline border-l-4 md:ml-4 pl-2 md:pl-4 mb-2 md:mb-0 border-[#3d3d3d]">
                   {career.position}
                 </span>
               </h2>
@@ -50,7 +50,7 @@ export default function Careers() {
                   {startDate} - {endDate ? endDate : <i>Present</i>}
                 </span>
               </div>
-              <div className="mt-4 pl-1 max-w-[700px]">
+              <div className="mt-1 pl-1 max-w-[700px]">
                 <ul className="list-disc">
                   {career.jobs.map((job, index) => {
                     return (
