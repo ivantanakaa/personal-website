f2c15fdf22c4f10395ee5421f93bebbca42ae402
Ivan Tanaka <ivantanaka@Ivans-MacBook-Air.local>
2024-04-12 11:15:09 +0700

    feat: adding Download Resume Text in desktop view

--- src/app/components/Download/index.tsx
+++ src/app/components/Download/index.tsx
@@ -5,19 +5,22 @@ export default function Download() {
   return (
     <div className="fixed right-2 xl:right-10 bottom-2 xl:bottom-10">
       <Link
-        download={"Ivan Tanaka CV"}
+        download={"Ivan Tanaka Resume"}
         target="_blank"
-        href="/assets/pdf/Ivan Tanaka CV.pdf"
+        href="/assets/pdf/Ivan Tanaka Resume.pdf"
         prefetch={false}
         locale={false}
       >
-        <div className="bg-[#3d3d3d] rounded-full p-2">
+        <div className="bg-[#3d3d3d] rounded-full p-2 md:px-4 flex flex-row items-center">
           <Image
             alt="download pdf"
             src="/assets/images/file-download-icon.svg"
             width={35}
             height={35}
           />
+          <div className="font-semibold text-[#ecf0f1] ml-2 hidden md:block">
+            Download Resume
+          </div>
         </div>
       </Link>
     </div>
