a3da25ad7f26c01250afbb74d52d5474ecf8a738
Ivan Tanaka <ivantanaka@Ivans-MacBook-Air.local>
2024-09-02 12:51:53 +0700

    feat: adding Super Mentor in Honors and styling Recommendations section

--- src/app/_modules/Recommendations/index.tsx
+++ src/app/_modules/Recommendations/index.tsx
@@ -1,14 +1,13 @@
 import recommendations from "./recommendations.json";
 export default function Recommendations() {
-  const renderRecommendations = () =>{
+  const renderRecommendations = () => {
     return recommendations.map(({ name, title, description }, index) => {
       return (
-        <li
-          className="list-none flex items-stretch "
-          key={`honor-${index}`}
-        >
-          <div className="mb-4 mr-4 min-w-[80vw] flex bg-white flex-col items-center justify-center shadow-xl rounded-lg p-8 ">
-            <span className="text-xl font-semibold italic text-center">{name}</span>
+        <li className="list-none flex items-stretch " key={`honor-${index}`}>
+          <div className="mb-4 mr-4 min-w-[90vw] lg:min-w-[70vw] flex bg-white flex-col items-center justify-center shadow-xl rounded-lg p-8 ">
+            <span className="text-xl font-semibold italic text-center">
+              {name}
+            </span>
             <span className="text-xs text-gray-500 italic mt-1">{title}</span>
             <blockquote
               className="italic text-center mt-5"
@@ -17,8 +16,8 @@ export default function Recommendations() {
           </div>
         </li>
       );
-    })
-  }
+    });
+  };
   return (
     <section className="xl:p-y-6 p-y-4 flex flex-col items-center">
       <h1 className="text-txt-dark text-4xl mb-4 pb-4 border-b-4 px-4 w-fit border-txt-dark">
