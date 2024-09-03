b56e8ffddfde2ae08c572f3369cb7b21f7983475
Ivan Tanaka <ivantanaka@Ivans-MacBook-Air.local>
2024-09-03 18:44:51 +0700

    feat: adding snap in carousel

--- src/app/_modules/Recommendations/index.tsx
+++ src/app/_modules/Recommendations/index.tsx
@@ -3,13 +3,13 @@ export default function Recommendations() {
   const renderRecommendations = () => {
     return recommendations.map(({ name, title, description }, index) => {
       return (
-        <li className="list-none flex items-stretch " key={`honor-${index}`}>
-          <div className="mb-4 mr-4 min-w-[90vw] lg:min-w-[30vw] flex bg-white flex-col items-center justify-center shadow-xl rounded-lg p-8 ">
+        <li className="list-none flex items-stretch snap-start" key={`honor-${index}`}>
+          <div className="mb-4 mx-4 min-w-[90vw] lg:min-w-[30vw] flex bg-[#fefefe] flex-col items-center justify-center shadow-xl rounded-lg p-8 ">
             <span className="text-md font-semibold italic text-center">
               {name}
             </span>
             <hr className="w-full mt-2"/>
-            <span className="text-xs text-gray-500 italic mt-1">{title}</span>
+            <span className="text-xs text-gray-500 italic mt-1 text-center">{title}</span>
             <blockquote
               className="italic text-center mt-5 text-xs"
               dangerouslySetInnerHTML={{ __html: description }}
@@ -24,8 +24,8 @@ export default function Recommendations() {
       <h1 className="text-txt-dark text-4xl mb-4 pb-4 border-b-4 px-4 w-fit border-txt-dark">
         Recommendations
       </h1>
-      <div className="w-full overflow-x-scroll">
-        <ul className="list-disc my-4 flex flex-row gap-5 mx-4">
+      <div className="w-full overflow-x-scroll snap-x snap-mandatory no-scrollbar">
+        <ul className="list-disc my-4 flex flex-row gap-5">
           {renderRecommendations()}
         </ul>
       </div>
