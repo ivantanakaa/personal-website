81516319cd8ffa644ba640610c24347733e632cf
ivantanakaa <tanakaivann97@gmail.com>
2025-03-22 12:27:23 +0700

    feat: adding categories in skills

--- src/app/_modules/Skills/index.tsx
+++ src/app/_modules/Skills/index.tsx
@@ -1,15 +1,23 @@
 import skills from "./skills.json";
+
 export default function Skills() {
   return (
     <section className="xl:p-6 lg:px-12 xl:px-12 p-4 flex flex-col items-center">
       <h1 className="text-txt-dark text-4xl mb-4 pb-4 border-b-4 px-4 w-fit border-txt-dark">
         Skills
       </h1>
-      <ul className="list-disc grid md:grid-cols-4 grid-cols-3 grid-flow-row gap-y-4 gap-x-16 my-4">
-        {skills.map((data, index) => {
-          return <li key={`skill-${index}`}>{data}</li>;
-        })}
-      </ul>
+      <div className="flex flex-col gap-4">
+        {Object.entries(skills).map(([category, items]) => (
+          <div key={category} className="mb-4 w-full">
+            <h3 className="text-lg font-semibold mb-2 text-center">{category}</h3>
+            <ul className="list-disc grid md:grid-cols-4 sm:grid-cols-3 grid-cols-2 grid-flow-row gap-y-4 gap-x-16 my-4">
+              {items.map((item) => (
+                <li key={item}>{item}</li>
+              ))}
+            </ul>
+          </div>
+        ))}
+      </div>
     </section>
   );
 }
