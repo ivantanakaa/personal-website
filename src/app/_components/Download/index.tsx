a3ea7a16c7dd6a3efaccf7e584fa5eb1500e7d8d
ivantanakaa <tanakaivann97@gmail.com>
2025-03-22 18:02:22 +0700

    feat: revert to static resume

--- src/app/_components/Download/index.tsx
+++ src/app/_components/Download/index.tsx
@@ -1,50 +1,33 @@
 "use client";
 import { sendGAEvent } from "@next/third-parties/google";
 import Image from "next/image";
-import { useRef } from "react";
-import Resume from "../Resume";
-import html2pdf from "html2pdf.js";
-
+import Link from "next/link";
 
 export default function Download() {
-  const resumeRef = useRef(null);
-
-  const handleOnClickDownload = () => {
-    const element = resumeRef.current;
-    const options = {
-      margin: 5,
-      filename: "Ivan_Tanaka_Resume.pdf",
-      image: { type: "jpeg", quality: 0.98 },
-      html2canvas: { scale: 2,useCORS: true },
-      jsPDF: { unit: "mm", format: "a4", orientation: "portrait" },
-      pagebreak: { mode: ["avoid-all", "legacy"] },
-    };
-  
-    if (element) {
-      html2pdf().set(options).from(element).save();
-    }
-  };
-
   return (
     <div className="fixed right-2 xl:right-10 bottom-2 xl:bottom-10">
-      <div className="hidden">
-        <Resume resumeRef={resumeRef} />
-      </div>
-
-      <button
-        className="bg-[#3d3d3d] rounded-full p-2 md:px-4 flex flex-row items-center hover:opacity-75 hover:transition-all delay-150"
-        onClick={handleOnClickDownload}
+      <Link
+        download={"Ivan Tanaka Resume"}
+        target="_blank"
+        href="/assets/pdf/Ivan Tanaka Resume.pdf"
+        prefetch={false}
+        locale={false}
+        onClick={() => {
+          sendGAEvent("event", "click", { context: "download_resume" });
+        }}
       >
-        <Image
-          alt="download pdf"
-          src="/assets/images/file-download-icon.svg"
-          width={35}
-          height={35}
-        />
-        <div className="font-semibold text-[#ecf0f1] ml-2 hidden md:block">
-          Download Resume
-        </div>
-      </button>
+        <button className="bg-[#3d3d3d] rounded-full p-2 md:px-4 flex flex-row items-center hover:opacity-75 hover:transition-all delay-150">
+          <Image
+            alt="download pdf"
+            src="/assets/images/file-download-icon.svg"
+            width={35}
+            height={35}
+          />
+          <div className="font-semibold text-[#ecf0f1] ml-2 hidden md:block">
+            Download Resume
+          </div>
+        </button>
+      </Link>
     </div>
   );
 }
